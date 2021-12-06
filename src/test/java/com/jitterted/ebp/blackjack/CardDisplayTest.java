626e260007c3f319e293fe14b260c6b03877a32b
Ted M. Young <tedyoung@gmail.com>
2021-12-06 10:50:29 -0800

    LAB #1: Exposed Suit & Rank via "safe" Query Methods. Extracted display(), moving it to ConsoleCard.

--- src/test/java/com/jitterted/ebp/blackjack/CardDisplayTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/CardDisplayTest.java
@@ -9,7 +9,7 @@ class CardDisplayTest {
     public void displayTenAsString() throws Exception {
         Card card = new Card(Suit.CLUBS, Rank.TEN);
 
-        assertThat(card.display())
+        assertThat(ConsoleCard.display(card))
                 .isEqualTo("[30m┌─────────┐[1B[11D│10       │[1B[11D│         │[1B[11D│    ♣    │[1B[11D│         │[1B[11D│       10│[1B[11D└─────────┘");
     }
 
@@ -17,7 +17,7 @@ class CardDisplayTest {
     public void displayNonTenAsString() throws Exception {
         Card card = new Card(Suit.DIAMONDS, Rank.NINE);
 
-        assertThat(card.display())
+        assertThat(ConsoleCard.display(card))
                 .isEqualTo("[31m┌─────────┐[1B[11D│9        │[1B[11D│         │[1B[11D│    ♦    │[1B[11D│         │[1B[11D│        9│[1B[11D└─────────┘");
     }
 }
